90e406bfa65195da6ed9092fc68ec8b8a3c25ee6
leodenis <denisleo23@gmail.com>
2013-02-01 12:39:27 +0100

    Ajout bdd_UserController

--- app/Controllers/User_controller.php
+++ app/Controllers/User_controller.php
@@ -10,6 +10,27 @@ class User_controller{
 	}
 
 	function post(){
+		 F3::set('errorMsg',null);
+	    $check=array(
+	      'name'=>'required',
+	      'extension'=>'required'
+	    );
+	    $error=Datas::instance()->check(F3::get('POST'),$check);
+	    if($error){
+	      F3::set('errorMsg',$error);
+	    }
+	    else{	
+	    	echo 'OK';
+	    	//Ajout dans la base de donnée
+			$image=new DB\SQL\Mapper(F3::get('dB'),'image'); // Connexion à la table image
+			$image->copyFrom('POST'); // on récupère le POST
+			$image->save(); // on sauvegarde
+	      //$user=new App;
+	      //$user->record();
+	    }
+	}
+
+	function delete(){
 		
 	}
 
