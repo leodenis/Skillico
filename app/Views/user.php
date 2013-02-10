c62d9d48a5c2ea5227cd5e2a804b58d980d75aea
Leo D <denisleo23@gmail.com>
2013-02-11 00:08:32 +0100

    recup donnees relative a luser

--- app/Views/user.php
+++ app/Views/user.php
@@ -1,8 +1,12 @@
 
 <?php 
-
-
-
+// $tableauInfoUser=array_slice($InfoUserOffer, 0, 1);
+// $tableauInfoUserOffer=unset($InfoUserOffer[0]);
+$tableauInfoUser=array_shift($InfoUserOffer);
+echo '<pre>';
+print_r($tableauInfoUser);
+print_r($InfoUserOffer);
+echo '</pre>';
 if(F3::get('SESSION.user')){
  	echo 'Bienvenue ' .F3::get('SESSION.user').' ';
   echo '<a href="user/deconnexion">Déconnexion</a>';
@@ -12,7 +16,3 @@ else{
     echo '<a href="user/connexion">connexion</a>';
 }
  ?>
-
- <?php 
-echo $infoUserCo[0][firstname];
-   ?>
