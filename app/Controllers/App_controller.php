a1e1517d7e7d16dae409a3610af6a6416fe4d4d2
Guérot Simon <simon.guerot@gmail.com>
2013-02-24 20:05:11 +0100

    annonce_detail

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -16,6 +16,11 @@ class App_controller{
 		echo $view->render('monCompte.php');
 	}
 
+	function offer_detail(){
+		$view=new View(); 
+		echo $view->render('annonce_detail.html'); 
+	}
+
 	function __destruct(){
 
 	}
