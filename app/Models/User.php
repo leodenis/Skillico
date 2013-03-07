41e98b1b9dae3d0f95f4971fc62ca47c5511e3a7
Leo D <denisleo23@gmail.com>
2013-03-07 22:54:08 +0100

    comment user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,12 @@ class User extends Prefab{
 
 	}
 
+/**
+    Inscription
+    @param $password varchar
+    @param $born int
+    @return array
+**/
 	function inscription($password,$born){
 		   $img=Web::instance()->receive();
 				    if($img){
@@ -27,6 +33,20 @@ class User extends Prefab{
 			$users->save(); 
 	}
 
+/**
+    Inscription by FacebookConnect
+    @param $username varchar
+    @param $email varchar
+    @param $name varchar
+    @param $firstname varchar
+    @param $password varchar
+    @param $gender varchar
+    @param $city varchar
+    @param $born varchar
+    @param $imgProfil varchar
+    @return void
+    @return array
+**/
 	function inscriptionfb($username,$email,$name,$firstname,$password,$gender,$city,$born,$imgProfil){
 			$recupMdpId=new DB\SQL\Mapper(F3::get('dB'),'users');
 	    	$recupMdpId->afind(array('login=?',$username));
@@ -61,6 +81,13 @@ class User extends Prefab{
             }	
 	}
 
+/**
+    Forget Password
+    @param $login varchar
+    @param $password varchar
+    @return void
+    @return array
+**/
 	function forgetpassword($login,$password){
 			$users=new DB\SQL\Mapper(F3::get('dB'),'users'); // Connexion à la table image
 	 		$users->load(array('login=?',$login));
@@ -72,20 +99,48 @@ class User extends Prefab{
 
 	}
 
+/**
+	Connexion
+    @param $login varchar
+    @param $password varchar
+    @return array
+**/
 	function connexion($login,$password){
 			$recupMdpId=new DB\SQL\Mapper(F3::get('dB'),'users');
     		return $recupMdpId->afind(array('login=? and password=?',$login,$password));
 
 	}
 
+/**
+	Recover Information who is connect
+    @param $id int
+    @return array
+**/
+
 	function infoUserCo($id){
 			return $infoUserCo = F3::get('dB')->exec("SELECT U.id_users, U.login,U.name,U.firstname,U.email,U.adress,U.sexe,U.born,U.city,U.CP,U.phone,U.date_creation,U.last_connection,U.level,U.name,I.id_image,I.name as imageUser, I.extension FROM users AS U INNER JOIN image AS I ON U.fk_id_image = I.id_image WHERE U.id_users ='".$id."'");
 	}
+
+/**
+	Recover user avis 
+    @param $idUser int
+    @return array
+**/
 
 	function avis($idUser){
 			$avisUser=new DB\SQL\Mapper(F3::get('dB'),'avis');
     		return $avisUser->afind(array('fk_id_users=?',$idUser));
 	}
+
+/**
+	Edit Information of the user
+    @param $id int
+    @param $id_image int
+    @param $password varchar
+    @param $born int
+    @return void
+**/
+
 	function EditInfoUser($id,$id_image,$password,$born){
 
 			$img=Web::instance()->receive();
